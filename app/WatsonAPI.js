ac266d5b7abc01b442d536f09c67597fa87c2df4
Mike <mahathaway93@gmail.com>
2017-03-14 10:03:36 -0700

    added initial capacity for historical nyt articles

--- app/WatsonAPI.js
+++ app/WatsonAPI.js
@@ -1,22 +1,37 @@
 console.log('WatsonAPI Loaded')
 
-function textAnalysis() {
+//https://watson-api-explorer.mybluemix.net/apis/natural-language-understanding-v1#!/Analyze/analyzeGet
+
+//pass in nyt article as an additional argument to IIFE
+const textAnalysis = (function(){
+  const textAnalysis = {}
   const username = "5ce08e86-c1d1-4f72-821b-1bed6bb27ec4"
   const password = "BRmyPB5B6rgy"
-  // const url = `"http://galvanize-cors-proxy.herokuapp.com/https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&text=${}&features=sentiment,keywords`
 
-  $.ajax({
-      url: "http://galvanize-cors-proxy.herokuapp.com/https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&text=I%20still%20have%20a%20dream%2C%20a%20dream%20deeply%20rooted%20in%20the%20American%20dream%20%E2%80%93%20one%20day%20this%20nation%20will%20rise%20up%20and%20live%20up%20to%20its%20creed%2C%20%22We%20hold%20these%20truths%20to%20be%20self%20evident%3A%20that%20all%20men%20are%20created%20equal.&features=sentiment,keywords",
-      method: "GET",
+  // url: "http://galvanize-cors-proxy.herokuapp.com/https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&text=I%20still%20have%20a%20dream%2C%20a%20dream%20deeply%20rooted%20in%20the%20American%20dream%20%E2%80%93%20one%20day%20this%20nation%20will%20rise%20up%20and%20live%20up%20to%20its%20creed%2C%20%22We%20hold%20these%20truths%20to%20be%20self%20evident%3A%20that%20all%20men%20are%20created%20equal.&features=sentiment,keywords"
+  console.log(nytFunctionality.retreiveArticles())
 
-      beforeSend: function (xhr) {
-          xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
-      },
-  }).then(response => {
-      console.log(response);
-      console.log(response['sentiment']['document']);
+  textAnalysis.promiseChain = function(articleURL){
+    let url = 'http://galvanize-cors-proxy.herokuapp.com/https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&url='
+    // `${url}${articleURL}&features=sentiment`
 
-  }).catch(error => {
-      console.log('???', error);
-  })
-}
+    $.ajax({
+        url: 'http://galvanize-cors-proxy.herokuapp.com/https://watson-api-explorer.mybluemix.net/natural-language-understanding/api/v1/analyze?version=2017-02-27&text=hey%20there%20billy%2C%20Im%20a%20hero&features=sentiment&return_analyzed_text=false&clean=true&fallback_to_raw=true&concepts.limit=8&emotion.document=true&entities.limit=50&entities.emotion=false&entities.sentiment=false&keywords.limit=50&keywords.emotion=false&keywords.sentiment=false&relations.model=en-news&semantic_roles.limit=50&semantic_roles.entities=false&semantic_roles.keywords=false&sentiment.document=true',
+        method: "GET",
+
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+        },
+    }).then(response => {
+        console.log(response);
+        console.log(response['sentiment']['document']);
+
+    }).catch(error => {
+        console.log('???', error);
+    })
+  }
+
+  return textAnalysis
+})//()
+
+//,keywords
--- app/nytAPI.js
+++ app/nytAPI.js
@@ -4,6 +4,7 @@
 const nytFunctionality = (function(document){
   const nytFunctionality = {}
 
+  ///// This approach is depracated /////
   nytFunctionality.arrayOfAbstracts = function(data){
     return data.reduce((acc,curr) => {
       acc.push(curr.lead_paragraph)
@@ -19,29 +20,50 @@ const nytFunctionality = (function(document){
     })
   }
 
-  nytFunctionality.retreiveArticles = function(){
+  nytFunctionality.genArrayByParam = function(data,parameter){
+    return data.reduce((acc,curr) => {
+      acc.push(curr[`${parameter}`])
+      return acc
+    },[])
+  }
+
+//'http://api.nytimes.com/svc/search/v2/articlesearch.json?query=Trump&facets=publication_year&api-key=bee376d83aef4bdaa4a5591e1bd2be14'
+
+  nytFunctionality.retreiveArticles = function(searchString){
     let url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
-      'api-key': "bee376d83aef4bdaa4a5591e1bd2be14"
-    });
+      'api-key': "bee376d83aef4bdaa4a5591e1bd2be14",
+    }); //+ '&facet_field=source&facet_filter=true'; //recent addition
     $.ajax({
-      url: url,
+      url: url, //url,
       method: 'GET',
+      data: {
+        'q': searchString, //['Trump','Clinton'] -- would be cool to generalize to both
+        'begin_date': "20160101",
+        'end_date': "20161108"
+      }
     }).then(function(result) {
+      console.log(result,url)
       const articles = result.response.docs
+      const web_url = "web_url"
 
+      const articleURLs = nytFunctionality.genArrayByParam(articles,web_url)
       const TrumpArticles = nytFunctionality.searchForCandidate(nytFunctionality.arrayOfAbstracts(articles),'Trump')
       const ClintonArticles = nytFunctionality.searchForCandidate(nytFunctionality.arrayOfAbstracts(articles),'Clinton')
 
-      console.log(nytFunctionality.arrayOfAbstracts(articles))
-      console.log(TrumpArticles,ClintonArticles)
+      //insert call to watsonAPI here
+      console.log(articles)
+      // textAnalysis.promiseChain(articleURLs[1])
+
+      // console.log('!', articleURLs)
+      // console.log(nytFunctionality.arrayOfAbstracts(articles))
+      // console.log(TrumpArticles,ClintonArticles)
     }).catch(function(err) {
       throw err;
     });
-
   }
 
   return nytFunctionality
 })(document)
 
-nytFunctionality.retreiveArticles()
+nytFunctionality.retreiveArticles('Clinton')
